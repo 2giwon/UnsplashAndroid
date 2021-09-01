14cd47b34d4cba7ed4c2ffa058fad0974b5eeafa
middlefgr <2giwon@gmail.com>
2021-09-01 22:46:44 +0900

    Add - build Src

---
+++ app/src/main/java/com/egiwon/unsplashandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.egiwon.unsplashandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
