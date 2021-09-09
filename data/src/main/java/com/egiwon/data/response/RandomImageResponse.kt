0f48745f39cd9bb4ae4c7640bef9ee6dfbc770bb
middlefgr <2giwon@gmail.com>
2021-09-10 00:14:08 +0900

    Fix - default 인수값 추가

--- data/src/main/java/com/egiwon/data/response/RandomImageResponse.kt
+++ data/src/main/java/com/egiwon/data/response/RandomImageResponse.kt
@@ -1,22 +1,23 @@
 package com.egiwon.data.response
 
+import com.egiwon.domain.entity.Image
 import com.google.gson.annotations.SerializedName
 
 data class RandomImageResponse(
     @SerializedName("description")
-    val description: Any,
+    val description: Any = Any(),
     @SerializedName("id")
-    val id: String,
+    val id: String = "",
     @SerializedName("liked_by_user")
-    val likedByUser: Boolean,
+    val likedByUser: Boolean = false,
     @SerializedName("likes")
-    val likes: Int,
+    val likes: Int = 0,
     @SerializedName("urls")
-    val urls: Urls,
+    val urls: Urls = Urls(),
     @SerializedName("views")
-    val views: Int,
+    val views: Int = 0,
     @SerializedName("width")
-    val width: Int,
+    val width: Int = 0,
     @SerializedName("height")
-    val height: Int
+    val height: Int = 0
 )
