95b293da0bea2081ce84c768122aa7b1df960222
middlefgr <2giwon@gmail.com>
2021-09-10 00:10:51 +0900

    Fix - RandomImageService -> ImageDataSourceImpl 의존성 주입

--- data/src/main/java/com/egiwon/data/datasource/ImageDataSourceImpl.kt
+++ data/src/main/java/com/egiwon/data/datasource/ImageDataSourceImpl.kt
@@ -1,10 +1,12 @@
 package com.egiwon.data.datasource
 
 import com.egiwon.data.ImageDataSource
-import com.egiwon.data.api.RetrofitApi
 import com.egiwon.data.response.RandomImageResponse
+import javax.inject.Inject
 
-internal class ImageDataSourceImpl(private val randomImageService: RandomImageService) : ImageDataSource {
+internal class ImageDataSourceImpl @Inject constructor(
+    private val randomImageService: RandomImageService
+) : ImageDataSource {
     override suspend fun fetchRandomImages(): RandomImageResponse {
         return randomImageService.fetchRandomImage()
     }
