e02744f20dca60f06f6e6ed6117659d01b8329f2
middlefgr <2giwon@gmail.com>
2021-09-10 00:13:17 +0900

    Add - repository fetchimage 추가

--- data/src/main/java/com/egiwon/data/ImageRepositoryImpl.kt
+++ data/src/main/java/com/egiwon/data/ImageRepositoryImpl.kt
@@ -1,7 +1,16 @@
 package com.egiwon.data
 
+import com.egiwon.data.response.mapToImage
 import com.egiwon.domain.ImageRepository
+import com.egiwon.domain.entity.Image
+import javax.inject.Inject
 
-internal class ImageRepositoryImpl: ImageRepository {
+internal class ImageRepositoryImpl @Inject constructor(
+    private val imageDataSource: ImageDataSource
+) : ImageRepository {
+
+    override suspend fun fetchRandomImage(): Image {
+        return imageDataSource.fetchRandomImages().mapToImage()
+    }
 
 }
--- domain/src/main/java/com/egiwon/domain/ImageRepository.kt
+++ domain/src/main/java/com/egiwon/domain/ImageRepository.kt
@@ -1,4 +1,7 @@
 package com.egiwon.domain
 
+import com.egiwon.domain.entity.Image
+
 interface ImageRepository {
+    suspend fun fetchRandomImage(): Image
 }
