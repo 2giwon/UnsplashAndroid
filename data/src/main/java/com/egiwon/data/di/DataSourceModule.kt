95a3e4861a47a9b69b28a4f972959d927178e79a
middlefgr <2giwon@gmail.com>
2021-09-11 23:04:02 +0900

    Fix - Provides -> Binds로 변경

--- data/src/main/java/com/egiwon/data/di/DataSourceModule.kt
+++ data/src/main/java/com/egiwon/data/di/DataSourceModule.kt
@@ -3,6 +3,7 @@ package com.egiwon.data.di
 import com.egiwon.data.ImageDataSource
 import com.egiwon.data.datasource.ImageDataSourceImpl
 import com.egiwon.data.datasource.RandomImageService
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,10 +12,9 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DataSourceModule {
+abstract class DataSourceModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideImageDataSource(randomImageService: RandomImageService): ImageDataSource =
-        ImageDataSourceImpl(randomImageService)
+    abstract fun provideImageDataSource(randomImageService: RandomImageService): ImageDataSource
 }
