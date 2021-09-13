daa2dfaeb7212b36ea54a92f91e4d3b297c12a9a
middlefgr <2giwon@gmail.com>
2021-09-13 22:05:25 +0900

    Fix - Usecase internal 제거

--- domain/src/main/java/com/egiwon/domain/usecase/GetRandomImageUseCase.kt
+++ domain/src/main/java/com/egiwon/domain/usecase/GetRandomImageUseCase.kt
@@ -4,7 +4,7 @@ import com.egiwon.domain.ImageRepository
 import com.egiwon.domain.model.PhotoData
 import javax.inject.Inject
 
-internal class GetRandomImageUseCase @Inject constructor(
+class GetRandomImageUseCase @Inject constructor(
     private val imageRepository: ImageRepository
 ) {
     suspend fun getRandomImage(): PhotoData {
